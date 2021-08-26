af13067bc5086bdcba53e72e9686663978c76294
freetimezp <kraftukrnet@ukr.net>
2021-08-26 21:07:33 +0300

    php blog. query SELECT ALL to db

---
+++ app/database/db.php
@@ -0,0 +1,35 @@
+<?php
+
+require('connect.php');
+
+function tt($value) {
+    echo '<pre>';
+    print_r($value);
+    echo '</pre>';
+}
+
+function selectAll($table) {
+    global $pdo;
+
+    $sql = "SELECT * FROM $table";
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    $errInfo = $query->errorInfo();
+
+    if($errInfo[0] !== PDO::ERR_NONE) {
+        echo $errInfo[2];
+        exit();
+    }
+
+    $data = $query->fetchAll();
+
+    return $data;
+}
+
+tt(selectAll('users'));
+
+
+
+
