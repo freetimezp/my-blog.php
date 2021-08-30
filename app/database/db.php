51ce0cee0d2ad28fc2be548b6e609b58e4306ccf
freetimezp <kraftukrnet@ukr.net>
2021-08-30 21:12:12 +0300

    php blog. query INSERT

--- app/database/db.php
+++ app/database/db.php
@@ -81,13 +81,43 @@ function selectOne($table, $params = []) {
     return $data;
 }
 
-$params = [
-    'admin' => 1
+function insert($table, $params) {
+    global $pdo;
+
+    $i = 0;
+    $col = '';
+    $mask = '';
+    foreach ($params as $key => $value) {
+        if($i === 0) {
+            $col = $col . "$key";
+            $mask = $mask . "'" . $value . "'";
+        }else{
+            $col = $col . ", $key";
+            $mask = $mask . ", '" . $value . "'";
+        }
+
+        $i++;
+    }
+
+    $sql = "INSERT INTO $table ($col) VALUES($mask)";
+
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+
+    dbCheckError($query);
+}
+
+$arrData = [
+    'admin' => '0',
+    'username' => 'test_user_3',
+    'email' => 'test3@test.com',
+    'password' => '444'
 ];
 
+insert('users', $arrData);
 
-tt(selectOne('users', $params));
-//tt(selectAll('users', $params));
+
+
 
 
 
